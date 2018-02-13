3591902210b1920496360feac5ce079b607657c8
Eimutisg <eimutis.genys@gmail.com>
2018-02-13 16:20:52 +0000

    Final

--- SoftwareTestingApp/SoftwareTestingApp.Tests/CalcPremiumTests.cs
+++ SoftwareTestingApp/SoftwareTestingApp.Tests/CalcPremiumTests.cs
@@ -14,12 +14,10 @@ namespace SoftwareTestingApp.Tests
         [TestCase(57, "female", 0.375)]
         [TestCase(16, "female", 0)]
         [TestCase(16, "male", 0)]
-
         [TestCase(20, "male", 6.0)]
         [TestCase(57, "male", 0.75)]
         [TestCase(45, "nogender", 0.0)]
         [TestCase(55, "nogender", 0.0)]
-
         public void TestPremiumCalculator(int age, string gender, double expected)
         {
             var sut = new Insurance();
