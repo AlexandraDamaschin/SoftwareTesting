351d3b559a8b15e9bc22d040c485116a8af4e9a4
Eimutisg <eimutis.genys@gmail.com>
2018-02-13 15:25:54 +0000

    Sample Test for the Test

---
+++ SoftwareTestingApp/SoftwareTestingApp.Tests/CalcPremiumTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SoftwareTestingApp.Tests
+{
+    [TestFixture]
+    public class CalcPremiumTests
+    {
+        [Test]
+        public void TestTest()
+        {
+            var sut = new Insurance();
+
+            var result = sut.CalcPremium(100,"female");
+
+            Assert.That(result, Is.GreaterThan(0));
+        }
+    }
+}
